462992d534fd3bebcd584b810c0d0c4c1ce46cbf
Trần Thiện Thanh <230277046+thanh-devTech@users.noreply.github.com>
2025-11-15 17:59:46 +0700

    fix error null banner

--- IronSourceAdsBanner.cs
+++ IronSourceAdsBanner.cs
@@ -3,14 +3,15 @@ using Unity.Services.LevelPlay;
 public class IronSourceAdsBanner : MonoBehaviour
 {
     private LevelPlayBannerAd bannerAd;
-    public void CreateBannerAd(string id_banner_ads) {
+    public void CreateBannerAd(string id_banner_ads)
+    {
         var adConfig = new LevelPlayBannerAd.Config.Builder()
             .SetSize(LevelPlayAdSize.BANNER)
             .SetPosition(LevelPlayBannerPosition.TopCenter)
             .SetDisplayOnLoad(true)
             .SetRespectSafeArea(true)
             .Build();
-        
+
         bannerAd = new LevelPlayBannerAd(id_banner_ads, adConfig);
         bannerAd.OnAdLoaded += BannerOnAdLoadedEvent;
         bannerAd.OnAdLoadFailed += BannerOnAdLoadFailedEvent;
@@ -20,23 +21,29 @@ public class IronSourceAdsBanner : MonoBehaviour
         bannerAd.OnAdLeftApplication += BannerOnAdLeftApplicationEvent;
         bannerAd.OnAdExpanded += BannerOnAdExpandedEvent;
     }
-    public void LoadBannerAd() {
+    public void LoadBannerAd()
+    {
         bannerAd.LoadAd();
     }
-    public void ShowBannerAd() {
-        bannerAd.ShowAd();
+    public void ShowBannerAd()
+    {
+        if (bannerAd != null) bannerAd.ShowAd();
     }
-    public void HideBannerAd() {
-        bannerAd.HideAd();
+    public void HideBannerAd()
+    {
+        if (bannerAd != null) bannerAd.HideAd();
     }
-    public void DestroyBannerAd() {
+    public void DestroyBannerAd()
+    {
         bannerAd?.DestroyAd();
     }
-    
-    public void PauseAutoRefresh() {
+
+    public void PauseAutoRefresh()
+    {
         bannerAd.PauseAutoRefresh();
     }
-    public void ResumeAutoRefresh() {
+    public void ResumeAutoRefresh()
+    {
         bannerAd.ResumeAutoRefresh();
     }
 
@@ -44,11 +51,11 @@ public class IronSourceAdsBanner : MonoBehaviour
     {
         this.ShowBannerAd();
     }
-    public void BannerOnAdLoadFailedEvent(LevelPlayAdError ironSourceError) {}
-    public void BannerOnAdClickedEvent(LevelPlayAdInfo adInfo) {}
-    public void BannerOnAdDisplayedEvent(LevelPlayAdInfo adInfo) {}
-    public void BannerOnAdDisplayFailedEvent(LevelPlayAdInfo adInfo, LevelPlayAdError error){}
-    public void BannerOnAdCollapsedEvent(LevelPlayAdInfo adInfo) {}
-    public void BannerOnAdLeftApplicationEvent(LevelPlayAdInfo adInfo) {}
-    public void BannerOnAdExpandedEvent(LevelPlayAdInfo adInfo) {}
+    public void BannerOnAdLoadFailedEvent(LevelPlayAdError ironSourceError) { }
+    public void BannerOnAdClickedEvent(LevelPlayAdInfo adInfo) { }
+    public void BannerOnAdDisplayedEvent(LevelPlayAdInfo adInfo) { }
+    public void BannerOnAdDisplayFailedEvent(LevelPlayAdInfo adInfo, LevelPlayAdError error) { }
+    public void BannerOnAdCollapsedEvent(LevelPlayAdInfo adInfo) { }
+    public void BannerOnAdLeftApplicationEvent(LevelPlayAdInfo adInfo) { }
+    public void BannerOnAdExpandedEvent(LevelPlayAdInfo adInfo) { }
 }
